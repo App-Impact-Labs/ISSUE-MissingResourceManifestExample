210a47e1bd0a116ebae7ce2e500a57cac4c30e97
Admir Tuzović <admir.tuzovic@appimpact.ba>
2017-12-24 20:33:13 +0100

    Initial commit.

---
+++ src/AppImpact.I18n.AspNetCore.Common/Controllers/StaticResourcesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace AppImpact.I18n.AspNetCore.Common.Controllers
+{
+    public class StaticResourcesController : ControllerBase
+    {
+        IStringLocalizerFactory _factory;
+
+        public StaticResourcesController(IStringLocalizerFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            CultureInfo.CurrentUICulture = new CultureInfo("bs-Latn-BA");
+
+            IStringLocalizer localizer = null;
+
+            localizer = _factory.Create("Buttons", "AppImpact.AspNetCore");
+
+            IEnumerable<LocalizedString> lstrings = localizer.GetAllStrings();
+
+            JObject dict = new JObject();
+
+            foreach (LocalizedString lstring in lstrings)
+            {
+                dict.Add(lstring.Name, lstring.Value);
+            }
+
+            return Ok(dict);
+        }
+    }
+}
